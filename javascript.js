56eb1bc02f169b0d344b09dea38bcf0aa5ae401e
ZvAler2000 <aleric.zvonimir@gmail.com>
2025-02-03 02:35:53 +0100

    Finished Function Basics lesson and the 4 exercises.

--- javascript.js
+++ javascript.js
@@ -2,3 +2,11 @@ let string = "Zvone";
 let number = 420;
 let res = string + number;
 console.log(`Hello, ${res}!`);
+
+let func = (a, b) => a + b;
+
+function text(input) {
+    return "Hello, " + input + "!";
+}
+
+console.log(text(func(string, number)));
