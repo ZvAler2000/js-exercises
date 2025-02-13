21aeef995703c6525f8391aac63019cdd6cf9a9c
ZvAler2000 <aleric.zvonimir@gmail.com>
2025-02-13 04:52:52 +0100

    Done exerecises including array functions map, filter and reduce.

--- javascript.js
+++ javascript.js
@@ -62,3 +62,62 @@ function Fizzbuzz(num) {
     }
     return output;
 }
+
+function isEven(input) {
+    if(input % 2 == 0) {
+        return true;
+    }else {
+        return false;
+    }
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const fbArray = array.map(Fizzbuzz);
+const filterArray = array.filter(isEven);
+const thirdArray = array.filter((input) => input % 3 == 0);
+const reduceArray = array.reduce((initialValue, currentItem) => { return initialValue - currentItem; }, 0);
+let variable = 1;
+
+console.log(fbArray);
+console.log(filterArray);
+console.log(thirdArray);
+console.log(reduceArray);
+for(let i = 2; i < 16; i++) {
+    variable -= i;
+}
+console.log("Variable:" + variable);
+
+// let sum = 0;
+// const s3eMapArray = array.map((input) => {
+//     if(input % 2 == 0) {
+//         sum += input * 3;
+//     }
+// });
+// console.log("Map: " + sum);
+
+// let fsum = 0;
+// const s3eFilterArray = array.filter((input) => {
+//     if(isEven(input)) {
+//         fsum += input * 3;
+//     }
+// });
+// console.log("Filter: " + fsum);
+
+// const s3eReduceArray = array.reduce((total, currentItem) => {
+//     if(currentItem % 2 == 0) {
+//         console.log("BRUH");
+//         return total + (currentItem * 3);
+//     }
+//     console.log("yikes!");
+// }, 0);
+
+// console.log("Reduce: " + s3eReduceArray);
+
+function sumOfTripledEvens(array) {
+    return array
+        .filter((input) => input % 2 === 0)
+        .map((input) => input * 3)
+        .reduce((tot, curr) => tot + curr);
+}
+
+console.log(sumOfTripledEvens(array));
